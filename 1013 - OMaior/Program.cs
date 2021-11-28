ac64597c994cf2fdf6aa1cafe5db8214b132c29e
Luciana Lopes <lbragalopes@gmail.com>
2021-11-28 10:20:11 -0300

    Accepted beccrowd 1013

---
+++ 1013 - OMaior/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+
+        string[] num = Console.ReadLine().Split(' ');
+        int A = int.Parse(num[0]);
+        int B = int.Parse(num[1]);
+        int C = int.Parse(num[2]);
+
+        int MaiorAB = (A + B + Math.Abs(A - B)) / 2;
+        int Maior = (C + MaiorAB + Math.Abs(MaiorAB - C)) / 2;
+        Console.WriteLine(Maior + " eh o maior");
+        Console.ReadLine();
+
+    }
+
+}
