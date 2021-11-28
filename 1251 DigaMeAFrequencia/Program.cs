f9617de5c001fe5260878d75efb79e710bf79344
Luciana Lopes <lbragalopes@gmail.com>
2021-11-28 10:58:02 -0300

    1251  Presentation error

---
+++ 1251 DigaMeAFrequencia/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1251_DigaMeAFrequencia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string str = Console.ReadLine();
+            while (!string.IsNullOrWhiteSpace(str))
+            {
+                var charLinq =
+                    from ch in str
+                    group ch by ch into groupChar
+                    let g = new
+                    {
+                        ASCII = (int)groupChar.Key,
+                        Count = groupChar.Count()
+                    }
+                    orderby g.ASCII descending
+                    orderby g.Count
+                    select g;
+                foreach (var item in charLinq)
+                    Console.WriteLine(item.ASCII + " " + item.Count);
+                Console.WriteLine(" ");
+
+                str = Console.ReadLine();
+
+            }
+
+        }
+    }
+}
