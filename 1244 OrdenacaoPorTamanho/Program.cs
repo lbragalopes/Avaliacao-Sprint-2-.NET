c55ccceef1e97088fde3d0d4538390f4f7c5fa0d
Luciana Lopes <lbragalopes@gmail.com>
2021-11-28 10:50:18 -0300

    Accepted beecrowd 1244

---
+++ 1244 OrdenacaoPorTamanho/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1244_OrdenacaoPorTamanho
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+
+                String entrada = Console.ReadLine();
+                String[] valoresEntrada = entrada.Split(' ');
+                List<String> lista = new List<String>();
+                foreach (string item in valoresEntrada)
+                {
+                    lista.Add(item);
+                }
+                while (lista.Count > 0)
+                {
+                    String palavraMaior = "";
+                    foreach (string palavra in lista)
+                    {
+                        if (palavra.Length > palavraMaior.Length)
+                        {
+                            palavraMaior = palavra;
+                        }
+                    }
+                    Console.Write(palavraMaior);
+                    if (lista.Contains(palavraMaior))
+                    {
+                        lista.Remove(palavraMaior);
+                    }
+                    if (lista.Count > 0)
+                    {
+                        Console.Write(" ");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
