7b28c2dfc7e929e15696dbf6307abe8544e975e7
Luciana Lopes <lbragalopes@gmail.com>
2021-11-28 10:05:24 -0300

    Accepted

---
+++ 1009 - SalarioComBonus/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+
+        string nome = Console.ReadLine();
+        double salfixo = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        double vendas = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        double total = salfixo + (vendas * 0.15);
+        Console.WriteLine("TOTAL = R$ " + total.ToString("F2", CultureInfo.InvariantCulture));
+
+        Console.ReadLine();
+    }
+
+}
