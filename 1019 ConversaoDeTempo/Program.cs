37d2c173685136ecb14d8fa82815359182d71a0d
Luciana Lopes <lbragalopes@gmail.com>
2021-11-28 10:28:37 -0300

    Accepted becrowd 1019

---
+++ 1019 ConversaoDeTempo/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1019_ConversaoDeTempo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N, x, horas, minutos, segundos;
+
+            N = int.Parse(Console.ReadLine());
+
+            horas = N / 3600;
+            x = N % 3600;
+
+            minutos = x / 60;
+            segundos = x % 60;
+
+            Console.WriteLine(horas + ":" + minutos + ":" + segundos);
+
+        }
+
+    }
+}
