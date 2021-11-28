c1da0cbcbe5cb3ba6aab90a726f62eb7ec57c0fd
Luciana Lopes <lbragalopes@gmail.com>
2021-11-28 12:15:10 -0300

    Accepted beecrowd 1507

---
+++ 1507 Subsequencias/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1507_Subsequencias
+{
+    class Program
+    {
+              static void Main(string[] args)
+        {
+            int N;
+
+            N = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < N; i++)
+            {
+                string S = Console.ReadLine();
+                int Q = int.Parse(Console.ReadLine());
+
+                for (int j = 0; j < Q; j++)
+                {
+                    string query = Console.ReadLine();
+                    string sub = "";
+                    int queryPos = 0;
+                    foreach (var c in S)
+                    {
+                        if (c == query[queryPos])
+                        {
+                            sub += c;
+                            queryPos++;
+                            if (queryPos >= query.Length)
+                            {
+                                break;
+                            }
+                        }
+                    }
+                    if (sub.Equals(query))
+                    {
+                        Console.WriteLine("Yes");
+                    }
+                    else
+                    {
+                        Console.WriteLine("No");
+                    }
+                }
+
+            }
+        }
+    }
+}
