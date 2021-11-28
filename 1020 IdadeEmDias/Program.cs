3dbb3a8d7cce814af8f99ce4453e4b9682aded30
Luciana Lopes <lbragalopes@gmail.com>
2021-11-28 10:35:18 -0300

    Accepted beecrowd 1020

---
+++ 1020 IdadeEmDias/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1020_IdadeEmDias
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int idade, anos, meses, dias, x;
+
+            idade = int.Parse(Console.ReadLine());
+
+            anos = idade / 365;
+            x = idade % 365;
+            meses = x / 30;
+            dias = x % 30;
+
+            Console.WriteLine(anos + " ano(s)");
+            Console.WriteLine(meses + " mes(es)");
+            Console.WriteLine(dias + " dia(s)");
+        }
+
+    }
+
+}
