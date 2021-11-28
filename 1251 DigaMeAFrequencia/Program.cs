9d77ef443a40b191edf1c80d5da30f8d85e861b3
Luciana Lopes <lbragalopes@gmail.com>
2021-11-28 15:39:18 -0300

    Accepted beecrowd 1251

--- 1251 DigaMeAFrequencia/Program.cs
+++ 1251 DigaMeAFrequencia/Program.cs
@@ -10,29 +10,80 @@ namespace _1251_DigaMeAFrequencia
     {
         static void Main(string[] args)
         {
+            List<Frequencia> lista = new List<Frequencia>();
+            string entrada;
+            bool flag = false;
 
-            string str = Console.ReadLine();
-            while (!string.IsNullOrWhiteSpace(str))
+            while ((entrada = Console.ReadLine()) != null)
             {
-                var charLinq =
-                    from ch in str
-                    group ch by ch into groupChar
-                    let g = new
+                if (flag)
+                {
+                    Console.WriteLine("");
+                }
+                for (int i = 0; i < entrada.Length; i++)
+                {
+                    int counter = 0;
+                    char letter = entrada[i];
+                    foreach (char c in entrada)
                     {
-                        ASCII = (int)groupChar.Key,
-                        Count = groupChar.Count()
+                        if (c == letter)
+                        {
+                            counter++;
+                        }
                     }
-                    orderby g.ASCII descending
-                    orderby g.Count
-                    select g;
-                foreach (var item in charLinq)
-                    Console.WriteLine(item.ASCII + " " + item.Count);
-                Console.WriteLine(" ");
-
-                str = Console.ReadLine();
-
+                    entrada = entrada.Replace(letter, (char)0);
+                    Frequencia f = new Frequencia(letter, counter);
+                    lista.Add(f);
+                }
+                lista.Sort();
+                foreach (Frequencia frequencia in lista)
+                {
+                    if (frequencia.Ascii != 0)
+                    {
+                        Console.WriteLine(frequencia.Ascii + " " + frequencia.Quantidade);
+                    }
+                }
+                flag = true;
+                lista.Clear();
             }
+        }
+    }
 
+    class Frequencia : IComparable
+    {
+        public int Quantidade { get; set; }
+        public int Ascii { get; private set; }
+        public Frequencia(int ascii, int repetitions)
+        {
+            Quantidade = repetitions;
+            Ascii = ascii;
+        }
+        public int CompareTo(object obj)
+        {
+            if (obj == null)
+            {
+                return 1;
+            }
+            Frequencia otherTest = obj as Frequencia;
+            if (otherTest != null)
+            {
+                if (otherTest.Quantidade == this.Quantidade)
+                {
+                    if (otherTest.Ascii > this.Ascii)
+                    {
+                        return 1;
+                    }
+                    else
+                    {
+                        return -1;
+                    }
+                }
+                return this.Quantidade.CompareTo(otherTest.Quantidade);
+            }
+            else
+            {
+                throw new ArgumentException("Não é possível dizer a frequencia!!");
+            }
         }
     }
 }
